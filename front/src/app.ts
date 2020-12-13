d7b3f67b6165504cc7aba39f4971ec5e7c6da8d0
Aurélien Devaux <aurelien.devaux16@gmail.com>
2020-12-13 19:20:29 +0100

    feat(front): finalize templates

--- front/src/app.ts
+++ front/src/app.ts
@@ -1,28 +1,104 @@
 const express = require('express')
 const app = express()
-
-const {TwingEnvironment, TwingLoaderFilesystem} = require('twing')
-let twing = new TwingEnvironment(new TwingLoaderFilesystem('./templates'))
-
 app.use(express.static('public'))
 
+const fetchApi = require('node-fetch')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { TwingEnvironment, TwingLoaderFilesystem } = require('twing')
+let twing = new TwingEnvironment(new TwingLoaderFilesystem('./templates'))
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
-  // twing.render('index.twig', {'name': 'World'}).then((output) => {
-  //   res.end(output);
-  // });
+  fetchApi(`${process.env.HOST_URL}/api/matches`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': process.env.HOST_URL
+    },
+    body: JSON.stringify({
+      apiKey: process.env.API_KEY
+    })
+  }).then(response => {
+    response
+      .json()
+      .then(json => {
+        twing.render('index.twig', { matches: json.data }).then((output) => {
+          res.end(output);
+        });
+      })
+    ;
+  });
 })
 
-app.delete('/', (req, res) => {
-  res.send('traitement de la suppression')
+app.get('/delete/:matchId', (req, res) => {
+  fetchApi(`${process.env.HOST_URL}/api/delete-match`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': process.env.HOST_URL
+    },
+    body: JSON.stringify({
+      apiKey: process.env.API_KEY,
+      matchId: parseInt(req.params.matchId)
+    })
+  }).then(response => {
+    response
+      .json()
+      .then(res.redirect(302, '/'))
+    ;
+  });
 })
 
 app.get('/add', (req, res) => {
-  res.send('new match here')
+  fetchApi(`${process.env.HOST_URL}/api/teams`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': process.env.HOST_URL
+    },
+    body: JSON.stringify({
+      apiKey: process.env.API_KEY
+    })
+  }).then(response => {
+    response
+    .json()
+    .then(json => {
+      twing.render('add.twig', { names: json.data }).then((output) => {
+        res.end(output);
+      });
+    })
+    ;
+  });
 })
 
 app.post('/add', (req, res) => {
-  res.send('traitement de la création')
+  fetchApi(`${process.env.HOST_URL}/api/add-match`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': process.env.HOST_URL
+    },
+    body: JSON.stringify({
+      apiKey: process.env.API_KEY,
+      match: {
+        playDate: new Date(req.body.play_date),
+        homeTeam: req.body.home_team,
+        homeScore: parseInt(req.body.home_score),
+        awayTeam: req.body.away_team,
+        awayScore: parseInt(req.body.away_score)
+      }
+    })
+  }).then(response => {
+    response
+      .json()
+      .then(res.redirect(302, '/'))
+    ;
+  });
 })
 
 app.listen(3000)
